9c7cd3f994c22247fdf0e2967182fd419ba03cd7
Seth David Gyimah <esec26@gmail.com>
2022-09-19 09:58:59 +0000

    created component files

--- src/components/RegStudent/RegStudent.js
+++ src/components/RegStudent/RegStudent.js
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 import { GiMetalHand } from "react-icons/gi";
 
 function RegStudent({setCreateStudentResults}){
-    const [firstname, setFirstname] = useState("")
-    const [lastname, setLastname] = useState("")
+    const [first_name, setFirstname] = useState("")
+    const [last_name, setLastname] = useState("")
     const [dob, setDob] = useState("")
+    const [date_admitted, setDateadmitted] = useState("")
     const [gpa, setGpa] = useState("")
-    const [courseId, setCourseId] = useState("")
-    const [indexNumber, setIndexNumber] = useState("")
+    const [course_id, setCourseId] = useState("")
+    const [index_no, setIndexNumber] = useState("")
     const [level, setLevel] = useState("")
 
 
@@ -15,12 +16,13 @@ function RegStudent({setCreateStudentResults}){
     function formSubmit(e){
         e.preventDefault()
         const formdata = {
-            firstname,
-            lastname,
+            first_name,
+            last_name,
             dob,
+            date_admitted,
             gpa,
-            courseId,
-            indexNumber,
+            course_id,
+            index_no,
             level
         }
         console.log(formdata)
@@ -46,30 +48,30 @@ function RegStudent({setCreateStudentResults}){
             <h1>STUDENT REGISTRATION FORM</h1>
             <br/>
             <label>First name: </label>
-            <input type="text" placeholder="enter first name" value={firstname} onChange={(e) => setFirstname(e.target.value)}/>
+            <input type="text" placeholder="enter first name" value={first_name} onChange={(e) => setFirstname(e.target.value)}/>
             <br/>
             <label>Last name: </label>
-            <input type="text"  placeholder="enter last name" value={lastname} onChange={(e) => setLastname(e.target.value)}/>
+            <input type="text"  placeholder="enter last name" value={last_name} onChange={(e) => setLastname(e.target.value)}/>
             <br/>
             <label>Date of birth: </label>
             <input type="date"  placeholder="enter date of birth" value={dob} onChange={(e) => setDob(e.target.value)}/>
             <br/>
             <label>Date Admitted: </label>
-            <input type="date"  placeholder="enter date admitted" value={dob} onChange={(e) => setDob(e.target.value)}/>
+            <input type="date"  placeholder="enter date admitted" value={date_admitted} onChange={(e) => setDateadmitted(e.target.value)}/>
             <br/>
 
             <label>GPA: </label>
             <input type="integer"  placeholder="leave empty" value={gpa} onChange={(e) => setGpa(e.target.value)}/>
             <br/>
             <label>Course id: </label>
-            <select value={courseId} onChange={(e) => setCourseId(e.target.value)}>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
+            <select type="text" value={course_id} onChange={(e) => setCourseId(e.target.value)}>
+                <option>Business</option>
+                <option>Science</option>
+                <option>Gen_Arts</option>
             </select>
             <br/>
             <label>Index number: </label>
-            <input type="integer"  placeholder="index_number" value={indexNumber} onChange={(e) => setIndexNumber(e.target.value)}/>
+            <input type="integer"  placeholder="index_number" value={index_no} onChange={(e) => setIndexNumber(e.target.value)}/>
             <br/>
             <label>Level: </label>
             <input type="integer"  placeholder="level" value={level} onChange={(e) => setLevel(e.target.value)}/>
