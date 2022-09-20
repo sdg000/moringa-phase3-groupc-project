358ca94ee4dbdaf7604981e1e8721b6d96940803
Seth David Gyimah <esec26@gmail.com>
2022-09-20 01:27:54 +0000

    created GradeSubjectcomponent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRoute, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainComponent from "./components/MainContent/MainComponent";
@@ -7,23 +7,44 @@ import RegStudent from "./components/RegStudent/RegStudent";
 import CreateGrade from "./components/CreateGrade/CreateGrade"
 
 function App() {
+  // contain all student Instances
+  const [students, setStudents] = useState([])
+
   const [createStudentResults, setCreateStudentResults] = useState([])
 
+  
+
   // pass createStudentResults to <ShowStudent/> component
+  console.log(createStudentResults, "live in app")
+
+  //anytime system loads, fetch all Student Instances.
+  //use useEffect to load and save all students in app, pass to <CreateGrade/>  to be mapped and provide Student subjects based on their
+  useEffect(() =>{
+    fetch("http://localhost:9292/students")
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+      // console.log(data)
+      // pass data as props to <CreateGrade/>
+      setStudents(data)
+    })
+  }, [])
 
   return (
     <div className="App">
       <Navbar />
         <Routes>
           <Route exact path="/" element={<MainComponent/>}>
-
           </Route>
+
           <Route path="/register-students" element={<RegStudent setCreateStudentResults={setCreateStudentResults}/>}>
-
           </Route>
-          <Route path="//grade-subject" element={<CreateGrade/>}>
 
+          <Route path="/grade-subject" element={<CreateGrade students={students}/>}>
           </Route>
+
+
 
           
 
--- src/components/CreateGrade/CreateGrade.js
+++ src/components/CreateGrade/CreateGrade.js
@@ -1,74 +1,105 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
-function CreateGrade(){
-    // const [firstname, setFirstname] = useState("")
-    // const [lastname, setLastname] = useState("")
-    // const [dob, setDob] = useState("")
-    // const [gpa, setGpa] = useState("")
-    // const [courseId, setCourseId] = useState("")
-    // const [indexNumber, setIndexNumber] = useState("")
-    // const [level, setLevel] = useState("")
+// takes in all student's data as prop from <Apps/>
+function CreateGrade({students}){
+    const [subjects, setSubjects] = useState([])
+    const [academic_year, setAcademic_year] = useState("")
+    const [term, setTerm] = useState("")
+    const [exams_score, setExamsScore] = useState("")
+    const [subject_id, setSubject_id] = useState("") 
+    const [index_no, setIndexNo] = useState("")
+    const [level, setLevel] = useState("")
 
-
-    // takes props from App
-    // function formSubmit(e){
+    // function to find student's subjects and prefill subject_id form
+    function getSubjects(index){
+        setIndexNo(index)
+        let student = students.find(item => item.index_no === parseInt(index))
         
-    //     fetch("http://localhost:9292/create-student", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify({
-    //             FormData
-    //         }),
-    //     })
-    //     .then(function(response){
-    //         return response.json()
-    //     })
-    //     .then(function(data){
-    //         setCreateStudentResults(data)
-    //         console.log(data)
-    //     })
-    // }
+           let subjectLists = student.subjects.map((item) =>{
+                return(
+                    <option key={item.id}>{item.subject_name}</option>
+                )
+            })
+            setSubjects(subjectLists)
+        
+    }
+
+    // function to post grade to server
+    function submitGrade(e){
+        e.preventDefault()
+
+        const formdata = {
+            academic_year,
+            term,
+            exams_score,
+            subject_id,
+            index_no,
+            level
+        }
+        console.log(formdata)
+        
+        fetch("http://localhost:9292/grade-subject", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formdata),
+        })
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data)
+        })
+
+    }
+
+
+
 
     return(
-        <form>
+        <form onSubmit={submitGrade}>
             <h1>SUBJECT GRADING FORM</h1>
             <br/>
-            <select type="integer">
-                <option value="1">2020-21</option>
-                <option value="1">2021-22</option>
-                <option value="1">2022-23</option>
-                <option value="1">2023-24</option>
-                <option value="1">2024-25</option>
-                <option value="1">2025-26</option>
-                <option value="1">2026-27</option>
-                <option value="1">2027-28</option>
-                <option value="1">2029-30</option>
+            <label>Academic Year: </label>
+            <select type="integer" value={academic_year} onChange={(e) => setAcademic_year(e.target.value)}>
+                <option>2020-21</option>
+                <option>2021-22</option>
+                <option>2022-23</option>
+                <option>2023-24</option>
+                <option>2024-25</option>
+                <option>2025-26</option>
+                <option>2026-27</option>
+                <option>2027-28</option>
+                <option>2029-30</option>
             </select>
             <br/>
-            <select type="integer" >
-                <option value="1">1</option>
-                <option value="1">2</option>
-                <option value="1">3</option>
+            <label>Term: </label>
+            <select type="integer"  value={term} onChange={(e) => setTerm(e.target.value)} >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+            </select>
+            <br/>
+            <label>Index Number: </label>
+            <input type="integer"  value={index_no} placeholder="Index number"  onChange={(e) => getSubjects(e.target.value)}/>
+            <br/>
+            <label>Level: </label>
+            <select type="integer" value={level} onChange={(e) => setLevel(e.target.value)}>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+            </select>
+            <br/>
+            <label>Subject id: </label>
+            <select type="text" value={subject_id} onChange={(e) => setSubject_id(e.target.value)}>
+               {subjects}
             </select>
             <br/>
             <label>Exams score: </label>
-            <input type="integer"  placeholder="enter exams_score" />
+            <input type="integer"  value={exams_score} placeholder="enter exams_score" onChange={(e) => setExamsScore(e.target.value)}/>
             <br/>
-            <label>Index Number: </label>
-            <input type="integer"  placeholder="Index number" />
-            <br/>
-
-            <label>Level: </label>
-            <select type="integer" >
-                <option value="1">1</option>
-                <option value="1">2</option>
-                <option value="1">3</option>
-            </select>
-            <br/>
-
-            <input type="submit" value="Register Student" />
+            <input type="submit" value="Grade" />
         </form>
     )
 
